a119efbda10180cf0cbb0e809deb5a5fb7c5b1a5
snowtigersoft <snowtigersoft@126.com>
2016-11-15 14:23:51 +0800

    alerta UI config

--- config.js.sh
+++ config.js.sh
@@ -10,6 +10,22 @@ angular.module('config', [])
     'client_id'   : "$CLIENT_ID",
     'gitlab_url'  : "$GITLAB_URL",
 
-    'colors'      : {}
+    'colors'      : {
+      'severity': {
+        'critical'     : '#e4b9b9',
+        'major'        : '#f2dede',
+        'minor'        : '#f7ecb5',
+        'warning'      : '#fcf8e3',
+        'indeterminate': '#silver',
+        'cleared'      : '#c1e2b3',
+        'normal'       : '#c1e2b3',
+        'ok'           : '#c1e2b3',
+        'informational': '#c1e2b3',
+        'debug'        : '#afd9ee',
+        'security'     : '#d9edf7',
+        'unknown'      : '#silver'
+      },
+      'highlight': 'lightgray'
+    }
   });
 EOF
